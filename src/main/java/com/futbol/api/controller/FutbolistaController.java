679b53e3400e30433caef0949c912ec5eecc2a17
Jose Felix <torrestjfelix@gmail.com>
2024-04-12 22:59:30 -0500

    terminando los controladores, ahora lista con paginacion

--- src/main/java/com/futbol/api/controller/FutbolistaController.java
+++ src/main/java/com/futbol/api/controller/FutbolistaController.java
@@ -1,14 +1,28 @@
 package com.futbol.api.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.futbol.api.entity.Futbolista;
+import com.futbol.api.service.FutbolistaService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.awt.print.Pageable;
 
 @RestController
+@RequestMapping("futbolista")
+@AllArgsConstructor
 public class FutbolistaController {
+    private final FutbolistaService futbolistaService;
     @GetMapping
-    public String hola(Pageable pageable){
-        return "hola papi";
+    @ResponseStatus(HttpStatus.OK)
+    public Page<Futbolista> findAll(Pageable pageable){
+        return futbolistaService.findAll(pageable);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Futbolista> findById(@PathVariable Integer id){
+        return ResponseEntity.of(futbolistaService.findById(id));
     }
 }
